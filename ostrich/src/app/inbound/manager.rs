a032a9cd1bee2fd833d240ec1c8bea11e32153ba
snow2flying <snow2flying@gmail.com>
2022-04-30 09:14:51 -0600

    win: decrease waiting time after init tun

--- ostrich/src/app/inbound/manager.rs
+++ ostrich/src/app/inbound/manager.rs
@@ -96,7 +96,7 @@ impl InboundManager {
 
                         tokio::spawn(async move {
                             let _ = tun_rx.recv().await;
-                            std::thread::sleep(std::time::Duration::from_secs(7));
+                            std::thread::sleep(std::time::Duration::from_secs(2));
 
                             let gateway = cmd::get_default_ipv4_gateway().unwrap();
                             println!("gateway: {:?}", gateway);
