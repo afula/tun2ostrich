b64703332ed49a48799be04bd6365eb59f960c12
snow2flying <snow2flying@gmail.com>
2022-10-05 22:06:52 -0600

    build config for windows

--- netstack-lwip/build.rs
+++ netstack-lwip/build.rs
@@ -4,34 +4,6 @@ use std::{
     process::Command,
 };
 
-fn sdk_include_path_for(sdk: &str) -> String {
-    // sdk path find by `xcrun --sdk {iphoneos|macosx} --show-sdk-path`
-    let output = Command::new("xcrun")
-        .arg("--sdk")
-        .arg(sdk)
-        .arg("--show-sdk-path")
-        .output()
-        .expect("failed to execute xcrun");
-
-    let inc_path =
-        Path::new(String::from_utf8_lossy(&output.stdout).trim()).join("usr/include");
-
-    return inc_path.to_str().expect("invalid include path").to_string()
-}
-
-fn sdk_include_path() -> String {
-    let os = env::var("CARGO_CFG_TARGET_OS").unwrap();
-
-    if os == "ios" {
-        return sdk_include_path_for("iphoneos")
-    }
-    else if os == "macos" {
-        return sdk_include_path_for("macosx")
-    }
-
-    return "".to_string()
-}
-
 fn compile_lwip() {
     println!("cargo:rerun-if-changed=src/lwip");
     cc::Build::new()
@@ -70,20 +42,18 @@ fn compile_lwip() {
         // .file("src/lwip/core/ipv6/mld6.c")
         .file("src/lwip/core/ipv6/nd6.c")
         .file("src/lwip/custom/sys_arch.c")
+        .file("src/lwiperr.c") // MARKER BEGIN - END Fixing the Windows builds
         .include("src/lwip/custom")
         .include("src/lwip/include")
-        .include(sdk_include_path())
         .warnings(false)
         .flag_if_supported("-Wno-everything")
-        .compile("liblwip.a");
+        .compile("liblwip"); // MARKER BEGIN - END Remove lib suffix
 }
 
 fn generate_lwip_bindings() {
     println!("cargo:rustc-link-lib=lwip");
     // println!("cargo:rerun-if-changed=src/wrapper.h");
     println!("cargo:include=src/lwip/include");
-
-    let sdk_include_path = sdk_include_path();
 
     let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let os = env::var("CARGO_CFG_TARGET_OS").unwrap();
@@ -99,30 +69,40 @@ fn generate_lwip_bindings() {
         } else {
             ""
         })
-        .clang_arg(if sdk_include_path.is_empty() {
-            "".to_string()
+        .clang_arg(if arch == "aarch64" && os == "ios" {
+            // sdk path find by `xcrun --sdk iphoneos --show-sdk-path`
+            let output = Command::new("xcrun")
+                .arg("--sdk")
+                .arg("iphoneos")
+                .arg("--show-sdk-path")
+                .output()
+                .expect("failed to execute xcrun");
+            let inc_path =
+                Path::new(String::from_utf8_lossy(&output.stdout).trim()).join("usr/include");
+            format!("-I{}", inc_path.to_str().expect("invalid include path"))
         } else {
-            format!("-I{}", sdk_include_path)
+            "".to_string()
         })
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
 
     let mut out_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    out_path = out_path.join("src");
+    out_path = out_path.join("src/netstack");
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }
 
 fn main() {
+    // set::env::set_var("BINDINGS_GEN",true);
     let os = env::var("CARGO_CFG_TARGET_OS").unwrap();
-    if os == "ios" || os == "android" || os == "linux" || os == "macos" {
+    if os == "ios" || os == "android" || os == "linux" || os == "macos" || os == "windows" {
         compile_lwip();
     }
 
     if env::var("BINDINGS_GEN").is_ok()
-        && (os == "ios" || os == "android" || os == "linux" || os == "macos")
+        && (os == "ios" || os == "android" || os == "linux" || os == "macos" || os == "windows")
     {
         generate_lwip_bindings();
     }
